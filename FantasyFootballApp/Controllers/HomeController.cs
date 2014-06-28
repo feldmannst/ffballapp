f08bdd094b54980eae7d164891f7688f18dfd7ec
feldmannst <feldmannst@gmail.com>
2014-06-27 23:23:06 -0400

    Create home viewmodel

--- FantasyFootballApp/Controllers/HomeController.cs
+++ FantasyFootballApp/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FantasyFootballApp.Models;
 
 namespace FantasyFootballApp.Controllers
 {
     public class HomeController : Controller
     {
+        FFBallDatabaseEntities db = new FFBallDatabaseEntities();
         public ActionResult Index()
         {
-            return View();
+            HomeViewModel viewModel = new HomeViewModel();
+            var players = db.Rankings.Where(a => a.ExpertID == 5).OrderBy(a => a.Rank).ToList();
+            var playerList = players.Take(10).ToList();
+            viewModel.TopPlayers = playerList;
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ FantasyFootballApp/Models/MainViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FantasyFootballApp.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Ranking> TopPlayers { get; set; }
+    }
+}
